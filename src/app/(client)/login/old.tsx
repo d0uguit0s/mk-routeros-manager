fb033ec2eaeafd279bb22d176830dc5eb6b66036
d0ugit0s <douglas.s.o676@gmail.com>
2024-03-13 20:02:28 -0300

    Tela de login

---
+++ src/app/(client)/login/old.tsx
@@ -0,0 +1,12 @@
+// import { FormEvent } from 'react'
+
+import { UserLoginForm } from '@/components/user-login-form'
+
+export default function Login() {
+  // async function onSubmit(event: FormEvent<HTMLFormElement>) {
+  //   // Previne que a página recarregue
+  //   event.preventDefault()
+  // }
+
+  return <UserLoginForm />
+}
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,15 +1,21 @@
 import { NextAuthOptions } from 'next-auth'
+// eslint-disable-next-line import/no-named-as-default
 import CredentialsProvider from 'next-auth/providers/credentials'
+import GitHubProvider from 'next-auth/providers/github'
 
-import { PrismaAdapter } from '@auth/prisma-adapter'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
 
 import { prisma } from './prisma'
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_SECRET!,
+    }),
     CredentialsProvider({
-      name: 'Credentials',
+      name: 'credentials',
       credentials: {
         email: { label: 'email', type: 'text', placeholder: 'email@mail.com' },
         password: { label: 'Password', type: 'password' },
@@ -17,9 +23,14 @@ export const authOptions: NextAuthOptions = {
       },
       async authorize(credentials, req): Promise<any> {
         const user = { email: 'doug@mail.com', password: '102030', name: 'Douglas' }
-
+        console.log('authorize method: ', credentials)
         return user
       },
     }),
   ],
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.SECRET,
+  debug: process.env.NODE_ENV === 'development',
 }
